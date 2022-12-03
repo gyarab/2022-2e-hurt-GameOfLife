de12b94949462f9dc5878f84040279aa978d76fe
Rumburaq2 <93189782+Rumburaq2@users.noreply.github.com>
2022-12-03 12:28:42 +0100

    Update Main.java
    maze 10x10 generovani o 1 generaci snad funguje spravne i s "wrap" do nekonecna - testovano pomoci http://www.cuug.ab.ca/dewara/life/life.html - width 10, height 10, delay 2000

--- Main.java
+++ Main.java
@@ -1,44 +1,91 @@
 public class Main {
+    static int RADKY = 10;//potrebujeme velikosti bludiste z ComboBoxu
+    static int SLOUPCE = 10;
     public static void main(String[] args) {
         System.out.println("Hello world!");
         //Cell maze = new Cell[]{new Cell(0, 0, 0),};
 
+
         Cell[][] maze = {
-                { new Cell(0, 0, 1), new Cell(0,1,1),  new Cell(0,2,1)},
-                { new Cell(1, 0, 1), new Cell(1,1,1),  new Cell(1,2,1)},
-                { new Cell(2, 0, 1), new Cell(2,1,1),  new Cell(2,2,1)},
+                { new Cell(0, 0, 0), new Cell(0,1,0),  new Cell(0,2,0), new Cell(0,3,0), new Cell(0,4,0), new Cell(0,5,0), new Cell(0,6,0), new Cell(0,7,0), new Cell(0,8,0), new Cell(0,9,0)},
+                { new Cell(1, 0, 1), new Cell(1,1,0),  new Cell(1,2,0), new Cell(1,3,0), new Cell(1,4,0), new Cell(1,5,0), new Cell(1,6,0), new Cell(1,7,0), new Cell(1,8,0), new Cell(1,9,0)},
+                { new Cell(2, 0, 0), new Cell(2,1,0),  new Cell(2,2,0), new Cell(2,3,0), new Cell(2,4,0), new Cell(2,5,0), new Cell(2,6,0), new Cell(2,7,0), new Cell(2,8,0), new Cell(2,9,0)},
+                { new Cell(3, 0, 0), new Cell(3,1,0),  new Cell(3,2,0), new Cell(3,3,0), new Cell(3,4,0), new Cell(3,5,0), new Cell(3,6,0), new Cell(3,7,0), new Cell(3,8,0), new Cell(3,9,0)},
+                { new Cell(4, 0, 0), new Cell(4,1,0),  new Cell(4,2,0), new Cell(4,3,0), new Cell(4,4,0), new Cell(4,5,0), new Cell(4,6,0), new Cell(4,7,0), new Cell(4,8,0), new Cell(4,9,0)},
+                { new Cell(5, 0, 0), new Cell(5,1,0),  new Cell(5,2,0), new Cell(5,3,0), new Cell(5,4,0), new Cell(5,5,0), new Cell(5,6,0), new Cell(5,7,0), new Cell(5,8,0), new Cell(5,9,0)},
+                { new Cell(6, 0, 0), new Cell(6,1,0),  new Cell(6,2,0), new Cell(6,3,0), new Cell(6,4,0), new Cell(6,5,0), new Cell(6,6,0), new Cell(6,7,0), new Cell(6,8,0), new Cell(6,9,0)},
+                { new Cell(7, 0, 0), new Cell(7,1,0),  new Cell(7,2,0), new Cell(7,3,0), new Cell(7,4,0), new Cell(7,5,0), new Cell(7,6,0), new Cell(7,7,0), new Cell(7,8,0), new Cell(7,9,0)},
+                { new Cell(8, 0, 0), new Cell(8,1,0),  new Cell(8,2,0), new Cell(8,3,0), new Cell(8,4,0), new Cell(8,5,0), new Cell(8,6,0), new Cell(8,7,0), new Cell(8,8,0), new Cell(8,9,0)},
+                { new Cell(9, 0, 0), new Cell(9,1,0),  new Cell(9,2,0), new Cell(9,3,0), new Cell(9,4,0), new Cell(9,5,0), new Cell(9,6,0), new Cell(9,7,1), new Cell(9,8,1), new Cell(9,9,1)},
         };
+
+
+
+        //MRDAT
+        /*
+        Cell[][] maze = new Cell[RADKY][SLOUPCE];
+
+        for (int x = 0; x < RADKY; x++) {
+            for (int y = 0; y < SLOUPCE; y++) {
+                if((x == 5 && y == 5) || (x == 5 && y == 6) || (x == 5 && y == 7)) {
+                    maze[x][y] = new Cell(x, y, 1);
+                }
+                else {
+                    maze[x][y] = new Cell(x, y, 0);
+                }
+            }
+        }
+
+        Cell[][] maze2 = new Cell[RADKY][SLOUPCE];
+        for (int x = 0; x < RADKY; x++) {
+            for (int y = 0; y < SLOUPCE; y++) {
+                    maze[x][y] = new Cell(x, y, 0);
+            }
+        }
+
+         */
+
 
         Cell[][] maze2 = {
-                { new Cell(0, 0, 0), new Cell(0,1,0),  new Cell(0,2,0)},
-                { new Cell(1, 0, 0), new Cell(1,1,0),  new Cell(1,2,0)},
-                { new Cell(2, 0, 0), new Cell(2,1,0),  new Cell(2,2,0)},
+                { new Cell(0, 0, 0), new Cell(0,1,0),  new Cell(0,2,0), new Cell(0,3,0), new Cell(0,4,0), new Cell(0,5,0), new Cell(0,6,0), new Cell(0,7,0), new Cell(0,8,0), new Cell(0,9,0)},
+                { new Cell(1, 0, 0), new Cell(1,1,0),  new Cell(1,2,0), new Cell(1,3,0), new Cell(1,4,0), new Cell(1,5,0), new Cell(1,6,0), new Cell(1,7,0), new Cell(1,8,0), new Cell(1,9,0)},
+                { new Cell(2, 0, 0), new Cell(2,1,0),  new Cell(2,2,0), new Cell(2,3,0), new Cell(2,4,0), new Cell(2,5,0), new Cell(2,6,0), new Cell(2,7,0), new Cell(2,8,0), new Cell(2,9,0)},
+                { new Cell(3, 0, 0), new Cell(3,1,0),  new Cell(3,2,0), new Cell(3,3,0), new Cell(3,4,0), new Cell(3,5,0), new Cell(3,6,0), new Cell(3,7,0), new Cell(3,8,0), new Cell(3,9,0)},
+                { new Cell(4, 0, 0), new Cell(4,1,0),  new Cell(4,2,0), new Cell(4,3,0), new Cell(4,4,0), new Cell(4,5,0), new Cell(4,6,0), new Cell(4,7,0), new Cell(4,8,0), new Cell(4,9,0)},
+                { new Cell(5, 0, 0), new Cell(5,1,0),  new Cell(5,2,0), new Cell(5,3,0), new Cell(5,4,0), new Cell(5,5,0), new Cell(5,6,0), new Cell(5,7,0), new Cell(5,8,0), new Cell(2,9,0)},
+                { new Cell(6, 0, 0), new Cell(6,1,0),  new Cell(6,2,0), new Cell(6,3,0), new Cell(6,4,0), new Cell(6,5,0), new Cell(6,6,0), new Cell(6,7,0), new Cell(6,8,0), new Cell(6,9,0)},
+                { new Cell(7, 0, 0), new Cell(7,1,0),  new Cell(7,2,0), new Cell(7,3,0), new Cell(7,4,0), new Cell(7,5,0), new Cell(7,6,0), new Cell(7,7,0), new Cell(7,8,0), new Cell(7,9,0)},
+                { new Cell(8, 0, 0), new Cell(8,1,0),  new Cell(8,2,0), new Cell(8,3,0), new Cell(8,4,0), new Cell(8,5,0), new Cell(8,6,0), new Cell(8,7,0), new Cell(8,8,0), new Cell(8,9,0)},
+                { new Cell(9, 0, 0), new Cell(9,1,0),  new Cell(9,2,0), new Cell(9,3,0), new Cell(9,4,0), new Cell(9,5,0), new Cell(9,6,0), new Cell(9,7,0), new Cell(9,8,0), new Cell(9,9,0)},
         };
 
 
+
+        //vypiseme zacatecni stav
+        System.out.println("zacatecni konfigurace: ");
         Cell c;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
+        for (int i = 0; i < RADKY; i++) {
+            for (int j = 0; j < SLOUPCE; j++) {
                 c = maze[i][j];
                 /*
                 System.out.print(c.x);
                 System.out.print(" ");
                 System.out.print(c.y);
                 System.out.print(" ");
-
                  */
                 System.out.print(c.stav);
             }
             System.out.println();
         }
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
+        //projdeme cely maze - kazda bunka spravne zapis svuj novy stav do maze2
+        for (int i = 0; i < RADKY; i++) {
+            for (int j = 0; j < SLOUPCE; j++) {
                 c = maze[i][j];
-               // if(i == 1 && j == 0 ) {
-                    int p = SpocitejSousedy(c, maze);
-                 //   System.out.println(p);
-                //}
+                // if(i == 5 && j == 4 ) {
+                int p = SpocitejSousedy(c, maze);
+                //   System.out.println(p);
+              //  }
 
                 if(c.stav == 0 && p == 3){
                     Cell x = new Cell(i, j, 1);
@@ -63,21 +110,22 @@ public class Main {
                 }
 
 
+
+
             }
         }
         System.out.println();
         //probehlo celym polem - ted je dalsi generace ulozena v poli Maze2
         //vyrednrujeme maze2
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
+        for (int i = 0; i < RADKY; i++) {
+            for (int j = 0; j < SLOUPCE; j++) {
                 c = maze2[i][j];
                 /*
                 System.out.print(c.x);
                 System.out.print(" ");
                 System.out.print(c.y);
                 System.out.print(" ");
-
                  */
                 System.out.print(c.stav);
             }
@@ -93,8 +141,8 @@ public class Main {
         int sum = 0;
         for (int i = -1; i < 2; i++) {
             for (int j = -1; j < 2; j++) {
-                int col = (c.x + i + 3) % 3;
-                int row = (c.y + j + 3) % 3;
+                int col = (c.x + i + SLOUPCE) % SLOUPCE;
+                int row = (c.y + j + SLOUPCE) % SLOUPCE;
                 sum += maze[col][row].stav;
             }
         }
